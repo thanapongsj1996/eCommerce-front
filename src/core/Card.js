1e8cc1864906452544305d04a2baa7dc91241063
Thanapong <thanapongsj1996@gmail.com>
2019-06-16 05:01:17 +0700

    Show product image

---
+++ src/core/Card.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ShowImage from './ShowImage'
+
+const Card = ({ product }) => {
+
+    return (
+        <div className='col-4 mb-3'>
+            <div className='card'>
+                <div className='card-header'>{product.name}</div>
+                <div className='card-body'>
+                    <ShowImage item={product} url='product' />
+                    <p>{product.description}</p>
+                    <p>{product.price} ฿</p>
+                    <p>{product.sold}</p>
+                    <Link to='/'>
+                        <button className='btn btn-outline-primary my-2 mx-2'>
+                            View Product
+                        </button>
+                    </Link>
+                    <button className='btn btn-outline-warning my-2 mx-2'>
+                        Add to card
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
