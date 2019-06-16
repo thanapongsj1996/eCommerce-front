579b687a9684cb1204934089ffa2288a602a9aa6
Thanapong <thanapongsj1996@gmail.com>
2019-06-16 16:30:48 +0700

    Filter with array

--- src/core/CheckBox.js
+++ src/core/CheckBox.js
@@ -1,10 +1,29 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-const CheckBox = ({categories}) => {
+const CheckBox = ({ categories, handleFilters }) => {
 
-    return categories.map((c, i)=> (
+    const [checked, setChecked] = useState([])
+
+    const handleToggle = c => () => {
+
+        // find index in array (return -1 if not found)
+        const currentCategoryId = checked.indexOf(c)
+        const newCheckedCategoryId = [...checked]
+
+        if (currentCategoryId === -1) {
+            newCheckedCategoryId.push(c)
+        } else {
+            newCheckedCategoryId.splice(currentCategoryId, 1)
+        }
+
+        console.log(newCheckedCategoryId)
+        setChecked(newCheckedCategoryId)
+        handleFilters(newCheckedCategoryId)
+    }
+
+    return categories.map((c, i) => (
         <li className='list-unstyled' key={i}>
-            <input type='checkbox' className='form-check-input' />
+            <input onChange={handleToggle(c._id)} type='checkbox' className='form-check-input' />
             <label className='form-check-label'>{c.name}</label>
         </li>
     ))
--- src/core/Shop.js
+++ src/core/Shop.js
@@ -3,8 +3,14 @@ import Layout from './Layout'
 import Card from './Card'
 import { getCategories } from '../admin/apiAdmin'
 import CheckBox from './CheckBox'
+import RadioBox from './RadioBox'
+import { prices } from './fixedPrices'
 
 const Shop = () => {
+
+    const [myFilters, setMyFilters] = useState({
+        filters: { category: [], price: [] }
+    })
 
     const [categories, setCategories] = useState([])
     const [error, setError] = useState(false)
@@ -20,6 +26,32 @@ const Shop = () => {
             })
     }
 
+    const handleFilters = (filters, filterBy) => {
+        console.log('SHOP: ', filters, filterBy)
+
+        const newFilters = { ...myFilters }
+        newFilters.filters[filterBy] = filters
+
+        if (filterBy == 'price') {
+            let priceValue = handlePrice(filters)
+            newFilters.filters[filterBy] = priceValue
+        }
+
+
+        setMyFilters(newFilters)
+    }
+
+    const handlePrice = (value) => {
+        const data = prices
+        let array = []
+        for (let key in data) {
+            if (data[key]._id === parseInt(value)) {
+                array = data[key].array
+            }
+        }
+        return array
+    }
+
     useEffect(() => {
         init()
     }, [])
@@ -30,11 +62,16 @@ const Shop = () => {
                 <div className='col-4'>
                     <h4>Filter by categories</h4>
                     <ul>
-                        <CheckBox categories={categories} />
+                        <CheckBox categories={categories} handleFilters={filters => handleFilters(filters, 'category')} />
                     </ul>
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox prices={prices} handleFilters={filters => handleFilters(filters, 'price')} />
+                    </div>
+
                 </div>
                 <div className='col-8'>
-                    right
+                    {JSON.stringify(myFilters)}
                 </div>
             </div>
         </Layout>
